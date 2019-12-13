fa4088756cab66743b7c0629caf72bb83f50263d
Ankit Tibadiya <tibadiaankit@ymail.com>
2019-12-12 21:27:28 -0500

    logo decoration

--- postit/src/components/layout/Navbar.js
+++ postit/src/components/layout/Navbar.js
@@ -2,19 +2,27 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import Loggedin from "./Loggedin";
 import Loggedout from "./Loggedout";
-import { connect } from 'react-redux';
+import { connect } from "react-redux";
+import postIticon from "./postIt.png";
 
 class Navbar extends Component {
   render() {
-    const { auth, profile } = this.props
-    const  links  = auth.uid ? <Loggedin profile={ profile } /> : <Loggedout />;
+    const { auth, profile } = this.props;
+    const links = auth.uid ? <Loggedin profile={profile} /> : <Loggedout />;
     return (
       <nav className="nav-wrapper grey darken-3">
         <div className="container">
           <Link to="/" className="brand-logo">
+            <img
+              src={postIticon}
+              alt="icon"
+              className="circle responsive-img"
+              height="35px"
+              width="35px"
+            />{" "}
             postIT
           </Link>
-          { auth.isLoaded && links }
+          {auth.isLoaded && links}
         </div>
       </nav>
     );
@@ -25,6 +33,6 @@ const mapStateToProps = state => {
   return {
     auth: state.firebase.auth,
     profile: state.firebase.profile
-  }
-}
-export default connect(mapStateToProps,null)(Navbar);
+  };
+};
+export default connect(mapStateToProps, null)(Navbar);
