860e51484c87e92a98038c1016712145c61f5053
Ankit Tibadiya <tibadiaankit@ymail.com>
2019-12-16 11:43:48 -0500

    Update post
    edit post will have all the fields intact before user updates any one of it

--- postit/src/components/posts/PostEdit.js
+++ postit/src/components/posts/PostEdit.js
@@ -7,7 +7,9 @@ import { compose } from "redux";
 class PostEdit extends Component {
   state = {
     title: "",
-    content: ""
+    content: "",
+    imageurl: "",
+    videourl: ""
   };
   handleChange = e => {
     this.setState({
@@ -15,62 +17,100 @@ class PostEdit extends Component {
       [e.target.id]: e.target.value
     });
   };
-  // handleSubmit = e => {
-  //   e.preventDefault();
-  //   this.props.updatePost(this.state, this.props.id);
-  //   this.props.history.push(`/post/${this.props.id}`);
-  // };
   handleSubmit = e => {
     e.preventDefault();
     this.props.updatePost(this.state, this.props.id);
     this.props.history.push(`/post/${this.props.id}`);
   };
+  cancelUpdate = e => {
+    this.props.history.push(`/post/${this.props.id}`);
+  };
 
   render() {
-    console.log("update", this.props);
-    return (
-      <div className="container padding">
-        <form onSubmit={this.handleSubmit} className="updatePost">
-          <h5 className="white-text text-darken-3">Update post</h5>
-          <div className="row">
-            <div className="input-field col s6">
-              <label htmlFor="title"></label>
-              <input
-                type="text"
-                data-length="300"
-                id="title"
-                className="title white-text"
-                onChange={this.handleChange}
-                placeholder="Title"
-                required
-              />
+    const { post } = this.props;
+    console.log("update", post);
+    if (post) {
+      return (
+        <div className="container padding">
+          <form onSubmit={this.handleSubmit} className="updatePost">
+            <h5 className="white-text text-darken-3">Update post</h5>
+            <div className="row">
+              <div className="input-field col s6">
+                <label htmlFor="title"></label>
+                <input
+                  type="text"
+                  data-length="300"
+                  id="title"
+                  className="title white-text"
+                  onChange={this.handleChange}
+                  // placeholder="Title"
+                  defaultValue={post.title}
+                  required
+                />
+              </div>
             </div>
-          </div>
-          <div className="row">
-            <div className="input-field">
-              <label htmlFor="content"></label>
-              <textarea
-                type="text"
-                id="content"
-                className="materialize-textarea white-text"
-                onChange={this.handleChange}
-                placeholder="Text(optional)"
-              />
+            <div className="row">
+              <div className="input-field">
+                <label htmlFor="content"></label>
+                <textarea
+                  type="text"
+                  id="content"
+                  className="materialize-textarea white-text"
+                  onChange={this.handleChange}
+                  placeholder="Text(optional)"
+                  defaultValue={post.content}
+                />
+              </div>
             </div>
-          </div>
-          <div className="row">
-            <div className="input-field">
-              <button className="btn blue lighten-1 blue z-depth-0">
-                Update
-              </button>
-              <button className="btn blue lighten-1 blue z-depth-0">
-                Cancel
-              </button>
+            <div className="row">
+              <div className="input-field">
+                <label htmlFor="content"></label>
+                <textarea
+                  type="text"
+                  id="imageurl"
+                  className="materialize-textarea white-text"
+                  onChange={this.handleChange}
+                  placeholder="Image url (optional)"
+                  defaultValue={post.imageurl}
+                />
+              </div>
             </div>
-          </div>
-        </form>
-      </div>
-    );
+            <div className="row">
+              <div className="input-field">
+                <label htmlFor="content"></label>
+                <textarea
+                  type="text"
+                  id="videourl"
+                  className="materialize-textarea white-text"
+                  onChange={this.handleChange}
+                  placeholder="Video url(optional)"
+                  defaultValue={post.videourl}
+                />
+              </div>
+            </div>
+            <div className="row">
+              <div className="input-field">
+                <button className="btn blue lighten-1 blue z-depth-0">
+                  Update
+                </button>
+                <button
+                  className="btn blue lighten-1 blue z-depth-0"
+                  onClick={this.cancelUpdate}
+                >
+                  Cancel
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+      );
+    } else {
+      return (
+        <div className="container center ">
+          <p>Loading post...</p>
+        </div>
+      );
+    }
   }
 }
 
